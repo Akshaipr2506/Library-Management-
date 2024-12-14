36c4219bf7e4e57f2f26918c8fb462e252f47421
Navii02 <naveenshaji02@gmail.com>
2024-12-14 19:08:44 +0530

    corrected

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import {  Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
-
+import Userhome from './pages/Userhome';
 function App() {
   return (
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path={'/'} element={<Userhome/>}></Route>
+        <Route path={'/'} element={<Userhome/>}/>
       </Routes>
   );
 }
